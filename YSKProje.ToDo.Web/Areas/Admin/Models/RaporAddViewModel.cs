7b68e1d115b750ab6ece49c5b6f63de80745c0c8
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2021-03-24 23:39:36 +0300

    member işlemleri

---
+++ YSKProje.ToDo.Web/Areas/Admin/Models/RaporAddViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using YSKProje.ToDo.Entities.Concrete;
+
+namespace YSKProje.ToDo.Web.Areas.Admin.Models
+{
+    public class RaporAddViewModel
+    {
+        [Display(Name = "Tanım:")]
+        [Required(ErrorMessage = "Tanım Alanı Zorunludur **")]
+        public string Tanim { get; set; }
+        [Display(Name = "Detay:")]
+        [Required(ErrorMessage = "Detay Alanı Zorunludur **")]
+        public string Detay { get; set; }
+        public  Gorev Gorev { get; set; }
+        public int GorevId { get; set; }
+    }
+}
--- YSKProje.ToDo.Web/Areas/Member/Controllers/IsEmriController.cs
+++ YSKProje.ToDo.Web/Areas/Member/Controllers/IsEmriController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using YSKProje.ToDo.Business.Interfaces;
 using YSKProje.ToDo.Entities.Concrete;
 using YSKProje.ToDo.Web.Areas.Admin.Models;
@@ -11,24 +12,25 @@ using YSKProje.ToDo.Web.Areas.Admin.Models;
 namespace YSKProje.ToDo.Web.Areas.Member.Controllers
 {
     [Area("Member")]
-    [Authorize(Roles="Member")]
+    [Authorize(Roles = "Member")]
     public class IsEmriController : Controller
     {
         private readonly IGorevService _gorevService;
-
-
-        public IsEmriController(IGorevService gorevService, IAppUserService appUserService)
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IRaporService _raporService;
+        public IsEmriController(IGorevService gorevService, UserManager<AppUser> userManager, IRaporService raporService)
         {
             _gorevService = gorevService;
-      
+            _userManager = userManager;
+            _raporService = raporService;
         }
-
-        public IActionResult Index( int id)
+        public async Task<IActionResult> Index()
         {
-  
             TempData["Active"] = "isemri";
-            var gorevler = _gorevService.GetirTumTablolarla(I => I.AppUserId == id && !I.Durum);
-            List<GorevListAllViewModel> models= new List<GorevListAllViewModel>();
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            //var gorevler = _gorevService.GetirTumTablolarla(I => I.AppUserId == id && I.Durum==false);
+            var gorevler = _gorevService.GetirTumTablolarla(I => I.AppUserId == user.Id && I.Durum == false);
+            List<GorevListAllViewModel> models = new List<GorevListAllViewModel>();
             foreach (var item in gorevler)
             {
                 GorevListAllViewModel model = new GorevListAllViewModel();
@@ -40,12 +42,19 @@ namespace YSKProje.ToDo.Web.Areas.Member.Controllers
                 model.OlusturulmaTarih = item.OlusturulmaTarih;
                 model.Raporlar = item.Raporlar;
                 model.AppUser = item.AppUser;
-                
                 models.Add(model);
-
-            
             }
             return View(models);
+        }
+
+        public IActionResult EkleRapor(int id)
+        {
+            TempData["Active"] = "isemri";
+            var gorev = _gorevService.GetirAciliyetIleId(id);
+            RaporAddViewModel model = new RaporAddViewModel();
+            model.GorevId = id;
+            model.Gorev = gorev;
+            return View(model);
         }
     }
 }
